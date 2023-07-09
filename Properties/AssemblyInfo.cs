2622f2469fd46a8c3df33e10ba7306da9302240a
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-07-09 14:45:34 -0400

    Update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("fpm - Flashpoint Component Manager")]
-[assembly: AssemblyDescription("Command-line component manager for Flashpoint.")]
+[assembly: AssemblyTitle("Flashpoint Manager (command-line)")]
+[assembly: AssemblyDescription("Command-line component manager for Flashpoint Archive")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("fpm - Flashpoint Component Manager")]
+[assembly: AssemblyProduct("Flashpoint Manager (command-line)")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
